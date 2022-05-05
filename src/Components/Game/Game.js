1a9e02f9d9796e836a1d88caeb74e59c4ab28a51
Rudy <rpalfray87@gmail.com>
2022-05-05 16:45:56 +0200

    Ajout d'un historique

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -7,27 +7,46 @@ class Game extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            squares : Array(9).fill(null),
+            history : [
+                Array(9).fill(null)
+            ],
+            step : 0,
             xPlayer : true,
             winner : null
         }
     }
 
     handleClick(i) {
-        let tab = this.state.squares.slice()
+        let history = this.state.history.slice(0, this.state.step + 1)
+        let tab = this.state.history[this.state.step].slice()
         if(!tab[i] && !this.state.winner) {
             this.state.xPlayer ? tab[i] = "X" : tab[i] = "O"
+            history.push(tab)
             this.calculateWinner(tab);
             this.setState({
-                squares : tab,
+                history : history,
+                step : history.length -1,
                 xPlayer : !this.state.xPlayer
             })
         } 
     }
 
+    goTo(step) {
+        this.setState({
+            step : step,
+            xPlayer : step % 2 === 0 ? true : false,
+            winner : null
+        })
+        console.log("hello")
+    }
+
     handleReset() {
         this.setState({
-            squares : Array(9).fill(null),
+            history : [
+                Array(9).fill(null)
+            ],
+            step : 0,
+            winner : null, 
             xPlayer : true
         })
     }
@@ -58,17 +77,26 @@ class Game extends Component {
         } else {
             statut = <h2>C'est à {this.state.xPlayer ? 'X' : 'O'} de jouer</h2>
         }
+
+        let moves = this.state.history.map( (x, i) => {
+            return (
+                <li key={i}>
+                    <button onClick={() => this.goTo(i)}>Mouvement {i + 1 }</button>
+                </li>
+            )
+        })
+
+        console.log(moves)
+        
         return (
             <div className={ Style.gamePanel }>
                 
                 <h1>Morpion</h1>
                 { statut }
                 <div>
-                    <Board squares={ this.state.squares } onClick={(i) => this.handleClick(i)} reset={() => this.handleReset()}/>
+                    <Board squares={ this.state.history[this.state.step] } onClick={(i) => this.handleClick(i)} reset={() => this.handleReset()}/>
                     <ul>
-                        <li>
-                            
-                        </li>
+                        { moves }
                     </ul>
                 </div>
             </div>
